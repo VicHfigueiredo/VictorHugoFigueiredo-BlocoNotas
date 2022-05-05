7b29fd0268ed4cf1e76609c750a3ef16fb19e2f9
victor.hfigueiredo <victor.hfigueiredo@SBC0505290W10-1>
2022-05-05 14:08:27 -0300

    Adicionar arquivos de projeto.

---
+++ BlocoNotas/BlocoNotas.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BlocoNotas
+{
+    public partial class BlocoNotas : Form
+    {
+        public BlocoNotas()
+        {
+            InitializeComponent();
+        }
+
+        private void BlocoNotas_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Title = "Abrir Arquivo";
+            openFileDialog1.FileName = "";
+            openFileDialog1.InitialDirectory = "C:\\";
+            openFileDialog1.Filter = "Arquivos txt|*.txt";
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                System.IO.StreamReader leitor = new System.IO.StreamReader(openFileDialog1.FileName);
+                rtxBoard.Text = leitor.ReadToEnd();
+                leitor.Close();
+            }
+        }
+
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            saveFileDialog1.Title = "Salvar Como";
+            saveFileDialog1.FileName = "";
+            saveFileDialog1.InitialDirectory = "C:\\";
+            saveFileDialog1.Filter = "Arquivos txt|*.txt";
+
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                System.IO.StreamWriter gravar = new System.IO.StreamWriter(saveFileDialog1.FileName);
+                gravar.Write(rtxBoard.Text);
+                gravar.Close();
+            }
+        }
+
+        private void newToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            rtxBoard.Text = string.Empty;
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void cutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (rtxBoard.Text == "")
+            {
+                MessageBox.Show("Marque algo.");
+            }
+            else
+            {
+                Clipboard.SetText(rtxBoard.SelectedText);
+                rtxBoard.SelectedText = "";
+            }
+        }
+
+        private void copyToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (rtxBoard.SelectedText == "")
+            {
+                MessageBox.Show("Marque algo.");
+            }
+            else
+            {
+                Clipboard.SetText(rtxBoard.SelectedText);
+            }
+        }
+
+        private void pasteToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            rtxBoard.SelectedText = Clipboard.GetText();
+        }
+
+        private void customizeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (fontDialog1.ShowDialog().Equals(DialogResult.OK))
+            {
+                rtxBoard.SelectionFont = fontDialog1.Font;
+            }
+        }
+
+        private void optionsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (colorDialog1.ShowDialog().Equals(DialogResult.OK))
+            {
+                rtxBoard.SelectionColor = colorDialog1.Color;
+            }
+        }
+    }
+}
